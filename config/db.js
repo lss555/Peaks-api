cbf9f93fa1370eff14cef0125d64e2beabc2822c
lss555 <chrisheibel7@gmail.com>
2020-08-05 13:34:47 -0600

    change config to Peaks API

--- config/db.js
+++ config/db.js
@@ -1,9 +1,11 @@
 'use strict'
 
 // creating a base name for the mongodb
-const mongooseBaseName = 'express-api-template'
+// express-api-template
+const mongooseBaseName = 'Peaks-api'
 
 // create the mongodb uri for development and test
+
 const database = {
   development: `mongodb://localhost/${mongooseBaseName}-development`,
   test: `mongodb://localhost/${mongooseBaseName}-test`
